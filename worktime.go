6d6104952c6503106255005a106b4a92384f7ce1
mugijiru <mugijiru.dev@gmail.com>
2018-07-25 09:54:36 +0900

    -h オプション用の文言追加

--- worktime.go
+++ worktime.go
@@ -8,6 +8,15 @@ import (
 )
 
 func main() {
+	// -hオプション用文言
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `
+Usage of %s:
+   %s [date]\n`, os.Args[0], os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
 	t := time.Now()
 	const format = "2006/01/02"
 	fmt.Printf("%v\t10:00\t19:00\n", t.Format(format))
