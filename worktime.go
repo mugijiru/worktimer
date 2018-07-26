e78f1f008aa6d958f1f079315d3a292c516c937f
mugijiru <mugijiru.dev@gmail.com>
2018-07-27 00:25:14 +0900

    引数指定で日付を取れるようにした

--- worktime.go
+++ worktime.go
@@ -12,12 +12,19 @@ func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, `
 Usage of %s:
-   %s [date]\n`, os.Args[0], os.Args[0])
+   %s [date]
+`, os.Args[0], os.Args[0])
 		flag.PrintDefaults()
 	}
 
 	flag.Parse()
-	t := time.Now()
+
 	const format = "2006/01/02"
+	t := time.Now()
+
+	if flag.Arg(0) != "" {
+		t, _ = time.Parse(format, flag.Arg(0))
+	}
+
 	fmt.Printf("%v\t10:00\t19:00\n", t.Format(format))
 }
